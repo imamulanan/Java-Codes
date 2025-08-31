3bde86edd8ca8fcd1b9a8e7122ed953cd32c7e3d
Imamul Kabir Anan <135373964+imamulanan@users.noreply.github.com>
2025-09-01 00:16:54 +0600

    Add files via upload

---
+++ Arrays/Pbm_01.java
@@ -0,0 +1,16 @@
+public class Pbm_01 {
+    public static void main(String[] args) {
+        int[] marks = new int[3];
+        marks[0]=98;
+        marks[1]=96;
+        marks[2]=92;
+
+       /*  System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);*/
+
+        for(int i=0;i<3;i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
