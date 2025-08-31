1a9b8dcb6b84363dbe12ac4c950e785a49ab3152
Imamul Kabir Anan <135373964+imamulanan@users.noreply.github.com>
2025-09-01 00:13:53 +0600

    Add files via upload

---
+++ Abstraction/Pbm_06.java
@@ -0,0 +1,41 @@
+// 6. We have to calculate the area of a rectangle, a square and a circle. Create an abstract class
+// 'Shape' with three abstract methods namely 'RectangleArea' taking two parameters,
+// 'SquareArea' and 'CircleArea' taking one parameter each. The parameters of
+// 'RectangleArea' are its length and breadth, that of 'SquareArea' is its side and that of
+// 'CircleArea' is its radius. Now create another class 'Area' containing all the three methods
+// 'RectangleArea', 'SquareArea' and 'CircleArea' for printing the area of rectangle, square
+// and circle respectively. Create an object of class 'Area' and call all the three methods.
+
+
+abstract class Shape{
+    abstract void RectangleArea(int length, int breadth);
+    abstract void SquareArea(int side);
+    abstract void CircleArea(int radius);
+}
+
+class Area extends Shape{
+    @Override
+    void RectangleArea(int length, int breadth){
+        System.out.println("Area of Rectangle:"+length*breadth);
+    }
+
+    @Override
+    void SquareArea(int side) {
+        System.out.println("Area of Square:"+side*side);
+    }
+
+    @Override
+    void CircleArea(int radius) {
+        System.out.println("Area of Circle:"+3.1416*radius*radius);
+    }
+}
+
+
+public class Pbm_06{
+    public static void main(String[] args) {
+        Area a = new Area();
+        a.RectangleArea(10, 20);
+        a.SquareArea(10);
+        a.CircleArea(10);
+    }
+}
