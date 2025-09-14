a1e90d66a3d24f96af8b5998b99c3803e507f6e1
Imamul Kabir Anan <135373964+imamulanan@users.noreply.github.com>
2025-09-14 23:55:06 +0600

    Add files via upload

---
+++ Inheritance/five.java
@@ -0,0 +1,37 @@
+/*
+ * Now repeat the above example to print the area of 15 squares.
+ Hint-Use array of objects
+
+
+ */
+class Rectangle {
+    Integer length;
+    Integer width;
+
+    public Rectangle(Integer length, Integer width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public Integer getArea() {
+        return length * width;
+    }
+
+    public Integer getPerimeter() {
+        return 2 * (length + width);
+    }
+}
+
+class Square extends Rectangle {
+    public Square(Integer side) {
+        super(side, side);
+    }
+}
+
+public class five {
+    public static void main(String[] args) {
+        Square square = new Square(5);
+        System.out.println("Square Area: " + square.getArea());
+        System.out.println("Square Perimeter: " + square.getPerimeter());
+    }
+}
